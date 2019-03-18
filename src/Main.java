8d611be85a8ad05e3d06944f4d7e30582b14b490
Joseph Scott <jbs198585@gmail.com>
2019-03-18 14:16:39 -0400

    solution and check

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean teenager = TeenNumberChecker.isTeen(13);
+        System.out.println("Are they a teen? True or false? " + teenager);
+    }
+
+}
